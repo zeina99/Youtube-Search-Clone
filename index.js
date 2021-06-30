0936a0325a4dd1b38f9994ec4affb68353a57506
Zeina <51930164+zeina99@users.noreply.github.com>
2021-06-30 12:23:21 +0400

    removed extra comments

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// Promise.all <-- look into using that instead of making seperate fetch calls and resolving one at a time. ex: displayChannelImgs
-// break this down into two scripts rather than one script
-
-// prod key
-// let key = "AIzaSyAj314hsQUuaB3n_Sebf_28uLfUpNr0boc";
-// testing key
-let key = "AIzaSyBzyrC-05gpXNc1HlLYV4igKKqGkDaP0cw";
+let key = "AIzaSyAj314hsQUuaB3n_Sebf_28uLfUpNr0boc";
 
 let searchButton = document.getElementById("search-button");
 let searchBar = document.getElementById("search-bar");
@@ -104,9 +98,6 @@ let displayResults = (results) => {
 let displayChannelImgs = (channelIds, displayChannelImgsOnDocument) => {
   // using a loop since the api skips repeated channel ids
   // repeated channel ids could be present if search results include more than one video for same channel
-
-  // const channelDataPromises = [];
-
   const channelDataPromises = channelIds.map((channelId) => {
     return fetch(
       "https://youtube.googleapis.com/youtube/v3/channels?" +
@@ -117,21 +108,6 @@ let displayChannelImgs = (channelIds, displayChannelImgsOnDocument) => {
         }).toString()
     );
   });
-
-  // for (let count = 0; count < channelIds.length; count++) {
-  //   let result = fetch(
-  //     "https://youtube.googleapis.com/youtube/v3/channels?" +
-  //       new URLSearchParams({
-  //         part: ["snippet", "contentDetails", "statistics"],
-  //         id: channelIds[count],
-  //         key: key,
-  //       }).toString()
-  //   );
-  //   channelDataPromises.push(result);
-
-  // .then((data) => data.json())
-  // .then((data) => displayChannelImgsOnDocument(data, count));
-  // }
 
   Promise.all(channelDataPromises).then((responses) =>
     responses.forEach((response, index) =>
@@ -262,139 +238,3 @@ let addCardClickListeners = () => {
     });
   }
 };
-
-// let fileName = location.pathname.split("/").slice(-1)[0];
-
-// // video page
-// if (fileName === "video.html") {
-//   let cardDivText = sessionStorage.getItem("cardInfo");
-//   let hiddenDivText = sessionStorage.getItem("hiddenInfo");
-//   document.onload = addVideoPageDivs(cardDivText, hiddenDivText);
-// }
-
-// function addVideoPageDivs(cardText, hiddenText) {
-//   [title, viewsAndPublishDate, channelTitle, _, description] =
-//     cardText.split("\n");
-
-//   // 3279943 views      at: 2018-07-16
-//   [likes, dislikes, videoId, channelThumbnailUrl] = hiddenText.split("\n");
-
-//   [views, publishDate] = viewsAndPublishDate.split("Published");
-
-//   let iframeDiv = document.getElementsByClassName("iframe-video")[0];
-
-//   let iframeWrapper = document.createElement("div");
-//   iframeWrapper.classList.add("iframe-wrapper");
-
-//   let iframeELement = document.createElement("iframe");
-//   iframeELement.src = "https://www.youtube.com/embed/" + videoId;
-
-//   iframeWrapper.appendChild(iframeELement);
-//   iframeDiv.appendChild(iframeWrapper);
-
-//   iframeDiv.appendChild(document.createElement("br"));
-
-//   let videoTitleElement = document.createElement("h2");
-//   videoTitleElement.textContent = title;
-//   iframeDiv.appendChild(videoTitleElement);
-
-//   iframeDiv.appendChild(document.createElement("br"));
-
-//   let videoDetails = document.createElement("div");
-//   let videoDetails1 = document.createElement("div");
-//   let videoDetails2 = document.createElement("div");
-
-//   videoDetails.classList.add("video-details");
-//   videoDetails1.classList.add("video-details1");
-//   videoDetails2.classList.add("video-details2");
-
-//   // create video details 1 content
-//   let viewsElement = document.createElement("span");
-//   viewsElement.textContent = views;
-
-//   let dateElement = document.createElement("span");
-//   dateElement.textContent = " - " + publishDate.split(":")[1];
-
-//   // add videodetails1 content to videodetails1
-//   videoDetails1.appendChild(viewsElement);
-//   videoDetails1.appendChild(dateElement);
-
-//   // create videodetails 2 content
-//   let likeIcon = document.createElement("span");
-//   let likesELement = document.createElement("span");
-//   let dislikesIcon = document.createElement("span");
-//   let dislikesElement = document.createElement("span");
-
-//   likeIcon.classList.add("material-icons-outlined", "md-24");
-//   likeIcon.textContent = "thumb_up";
-
-//   dislikesIcon.classList.add("material-icons-outlined");
-//   dislikesIcon.textContent = "thumb_down_alt";
-
-//   likesELement.textContent = likes;
-
-//   dislikesElement.textContent = dislikes;
-//   // add content to videodetails2
-//   videoDetails2.appendChild(likeIcon);
-//   videoDetails2.appendChild(likesELement);
-//   videoDetails2.appendChild(dislikesIcon);
-//   videoDetails2.appendChild(dislikesElement);
-//   // add video details 1 and 2 to videodetails
-//   videoDetails.appendChild(videoDetails1);
-//   videoDetails.appendChild(videoDetails2);
-
-//   iframeDiv.appendChild(videoDetails);
-
-//   // channel details
-//   createChannelDetails(
-//     iframeDiv,
-//     channelTitle,
-//     description,
-//     channelThumbnailUrl
-//   );
-// }
-
-// // channel details on video page
-// function createChannelDetails(
-//   iframeDiv,
-//   channelTitle,
-//   description,
-//   channelThumbnailUrl
-// ) {
-//   let channelDetailsDiv = document.createElement("div");
-//   channelDetailsDiv.classList.add("channel-details");
-//   iframeDiv.appendChild(channelDetailsDiv);
-
-//   let channelImgDiv = document.createElement("div");
-//   channelImgDiv.classList.add("channelImg");
-//   channelDetailsDiv.appendChild(channelImgDiv);
-
-//   let channelImg = document.createElement("img");
-//   channelImg.src = channelThumbnailUrl;
-//   // channelImg.classList.add("channelImg");
-//   channelImgDiv.appendChild(channelImg);
-
-//   let videoChannelInfoDiv = document.createElement("div");
-//   videoChannelInfoDiv.classList.add("video-channel-info");
-//   channelDetailsDiv.appendChild(videoChannelInfoDiv);
-
-//   let channelTitleElement = document.createElement("p");
-//   channelTitleElement.textContent = channelTitle;
-//   videoChannelInfoDiv.appendChild(channelTitleElement);
-
-//   let videoDescriptionElement = document.createElement("p");
-//   videoDescriptionElement.textContent = description;
-//   videoChannelInfoDiv.appendChild(videoDescriptionElement);
-
-//   // subscribe button
-//   let subscribeDiv = document.createElement("div");
-//   subscribeDiv.classList.add("subscribe");
-//   let subscribeButton = document.createElement("button");
-//   subscribeButton.textContent = "SUBSCRIBE";
-//   subscribeButton.id = "subscribe-button";
-
-//   subscribeDiv.appendChild(subscribeButton);
-
-//   channelDetailsDiv.appendChild(videoChannelInfoDiv);
-//   channelDetailsDiv.appendChild(subscribeDiv);
-// }
